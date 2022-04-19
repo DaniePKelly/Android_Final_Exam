03638022809d76d08f11eafbb24eb97425e8f4b3
DaniePKelly <Danielkellyisking@gmail.com>
2022-04-19 10:49:06 -0230

    Task 2 Complete

--- app/src/main/java/com/cna/mobileprogramming/movieBuff/ui/LandingActivity.kt
+++ app/src/main/java/com/cna/mobileprogramming/movieBuff/ui/LandingActivity.kt
@@ -1,12 +1,22 @@
 package com.cna.mobileprogramming.movieBuff.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.cna.mobileprogramming.foodie.R
 
 class LandingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+
+
+        val allMoviesBtn = findViewById<Button>(R.id.allMovieBtn)
+
+        allMoviesBtn.setOnClickListener(){
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
